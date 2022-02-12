413c1304437ae94636ff20f6f346948c80b002fa
UncleFed <nurfag@mail.ru>
2022-02-12 12:06:15 +0300

    Start commit of this project

---
+++ test_main_page.py
@@ -0,0 +1,10 @@
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
+
+# не забываем оставить пустую строку в конце файла
+
